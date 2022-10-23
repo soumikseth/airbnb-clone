55aa7446f7454010ddcf36df84abf618ce51b1a2
Soumik Seth <soumikseth28@gmail.com>
2022-10-23 13:40:38 +0530

    Commited

---
+++ build/precache-manifest.2496b6fd08e1c8163360019444e4cff0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93c8097ad29b85bce1a1044dc4ff81f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "beb12a5d1ea5c47aaeae",
+    "url": "/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "d1e06b574356c0d5f871",
+    "url": "/static/css/main.6777df22.chunk.css"
+  },
+  {
+    "revision": "beb12a5d1ea5c47aaeae",
+    "url": "/static/js/2.61919f7e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.61919f7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1e06b574356c0d5f871",
+    "url": "/static/js/main.89640109.chunk.js"
+  },
+  {
+    "revision": "9d9a4c99211d98368a0d",
+    "url": "/static/js/runtime-main.7b3982eb.js"
+  }
+]);
